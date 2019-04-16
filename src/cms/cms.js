3df335aaf359c1445711e84590dea87b16ef93d2
Ross Brown <ross@cremalab.com>
2019-04-16 11:14:07 -0500

    gutting starter, initial commit

---
+++ src/cms/cms.js
@@ -0,0 +1,9 @@
+import CMS from 'netlify-cms'
+
+import AboutPagePreview from './preview-templates/AboutPagePreview'
+import WorkPostPreview from './preview-templates/WorkPostPreview'
+import IndexPagePreview from './preview-templates/IndexPagePreview'
+
+CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('work', WorkPostPreview)
---
+++ src/templates/index-page.js
@@ -0,0 +1,82 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import WorkCollection from "../components/WorkCollection";
+
+export const IndexPageTemplate = ({
+  body
+}) => (
+  <div>
+    <div
+      className="full-width-image margin-top-0"
+    >
+      <div
+        style={{
+          display: "flex",
+          height: "150px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "left",
+          flexDirection: "column"
+        }}
+      >
+      </div>
+    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="content">
+                <div className="columns">
+                  <div className="column is-12">
+                    <p>{body}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+);
+
+IndexPageTemplate.propTypes = {
+  body: PropTypes.string,
+};
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <IndexPageTemplate
+        body={frontmatter.body}
+      />
+    </Layout>
+  );
+};
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object
+    })
+  })
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        body
+      }
+    }
+  }
+`;
