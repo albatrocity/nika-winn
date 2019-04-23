27fb181e3e50aed3289b4a44f36831106695e622
Ross Brown <ross@cremalab.com>
2019-04-23 10:34:19 -0500

    Adds gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,9 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-styled-components`
+    },
     "gatsby-plugin-netlify" // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
